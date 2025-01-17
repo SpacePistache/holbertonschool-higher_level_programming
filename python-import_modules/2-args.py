5a78572d58677a517165b876ccdd16bc68abaa29
SpacePistache <lukeholley@hotmail.com>
2025-01-17 14:16:32 +0100

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    num_arg = len(sys.argv) -1
+    num_arg = len(sys.argv) - 1
 if num_arg == 0:
     print(f"{num_arg} arguments.")
 elif num_arg == 1:
@@ -11,4 +11,3 @@ else:
 
 for i in range(1, len(sys.argv)):
     print(f"{i}: {sys.argv[i]}")
-
