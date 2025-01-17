2ef27aca2325f2b3909b515596203522da25b04c
SpacePistache <lukeholley@hotmail.com>
2025-01-17 14:15:00 +0100

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    print(sys.argv)
     num_arg = len(sys.argv) -1
 if num_arg == 0:
-    print(f"Number of argument(s) {num_arg} arguments:")
+    print(f"{num_arg} arguments.")
 elif num_arg == 1:
-    print(f"Number of argument(s) {num_arg} arguments:")
+    print(f"{num_arg} argument:")
 else:
-    print(f"Number of argument(s): {num_arg} arguments:")
+    print(f"{num_arg} arguments:")
 
 for i in range(1, len(sys.argv)):
     print(f"{i}: {sys.argv[i]}")
