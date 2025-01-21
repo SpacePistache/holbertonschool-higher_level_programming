bbcd4a408378b2243470065d8945c45f19476428
SpacePistache <lukeholley@hotmail.com>
2025-01-21 15:11:44 +0100

    8-multiple_returns.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-def add_tuple(tuple_a=(), tuple_b=()):
-
-    if len(tuple_a) < 2:
-        tuple_a = tuple_a + (0,) * (2 - len(tuple_a))
-
-    if len(tuple_b) < 2:
-        tuple_b = tuple_b + (0,) * (2 - len(tuple_b))
-
-    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+def multiple_returns(sentence):
+    # Check if the sentence is empty
+    if sentence == "":
+        return (0, None)
+    else:
+        return (len(sentence), sentence[0])
