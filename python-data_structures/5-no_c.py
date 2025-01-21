0d6aa5605a2d73ba1d960294141c6d9c63e0ac4b
SpacePistache <lukeholley@hotmail.com>
2025-01-21 14:04:05 +0100

    5-no_c.py

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    no_c_string = my_string({ord("c, C"): none})
-    print(no_c_string)
+    no_c_string = ""
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            no_c_string += i
+    return no_c_string
