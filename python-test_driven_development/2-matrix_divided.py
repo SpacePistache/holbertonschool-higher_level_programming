b7d64a0c868b6c59a0d98b413d951beec5192502
SpacePistache <lukeholley@hotmail.com>
2025-01-25 17:38:42 +0100

    2-matrix_divided.py

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
 def matrix_divided(matrix, div):
-    """Divides all elements of a matrix rounding to 2 decimal places."""
-
-    if not isinstance(matrix, list):
+    """
+    Divides elements of matrix by div, returns new matrix with results.
+    """
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a matrix of integers/floats")
-
-    for row in matrix:
-        if not isinstance(row, list):
-            raise TypeError("matrix must be a matrix of integers/floats")
-
-        for item in row:
-            if not isinstance(item, (int, float)):
-                raise TypeError("matrix must be a matrix of integers/floats")
+    if len(matrix) == 0 or any(len(row) == 0 for row in matrix):
+        raise TypeError("matrix must be a matrix of integers/floats")
+    if not all(isinstance(item, (int, float)) for row in matrix for item in row):
+        raise TypeError("matrix must be a matrix of integers/floats")
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
 
     row_length = len(matrix[0])
     for row in matrix:
         if len(row) != row_length:
-            raise TypeError("Each row of the matrix must have the same size")
+            raise TypeError("Each row of the matrix must be same size")
 
-    if not isinstance(div, (int, float)):
-        raise TypeError("div must be a number")
+    new_matrix = []
+    for row in matrix:
+        new_row = [round(item / div, 2) for item in row]
+        new_matrix.append(new_row)
 
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
-
-    return [
-        [round(item / div, 2) for item in row] for row in matrix
-    ]
+    return new_matrix
