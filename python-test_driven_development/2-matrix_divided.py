125886c84908f5e5320340523112f6cbc50810b7
SpacePistache <lukeholley@hotmail.com>
2025-01-25 17:15:33 +0100

    2-matrix_divided.py

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+def matrix_divided(matrix, div):
+    """Divides all elements of a matrix rounding to 2 decimal places."""
+
+    if not isinstance(matrix, list):
+        raise TypeError("matrix must be a matrix of integers/floats")
+
+    for row in matrix:
+        if not isinstance(row, list):
+            raise TypeError("matrix must be a matrix of integers/floats")
+
+        for item in row:
+            if not isinstance(item, (int, float)):
+                raise TypeError("matrix must be a matrix of integers/floats")
+
+    row_length = len(matrix[0])
+    for row in matrix:
+        if len(row) != row_length:
+            raise TypeError("Each row of the matrix must have the same size")
+
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    return [
+        [round(item / div, 2) for item in row] for row in matrix
+    ]
