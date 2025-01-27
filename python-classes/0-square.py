964d46282c0f07dd7a8d2b9d38ad4e00947d3e33
SpacePistache <lukeholley@hotmail.com>
2025-01-27 17:43:59 +0100

    0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 """ This module will provide functionality for working with a square """
-class square: 
-	""" An empty class for a square """
-	pass 
+
+
+class square:
+    """ An empty class for a square """
+
+
+pass
