8651884dc414529f3b047e52cee3830180dcd85f
SpacePistache <lukeholley@hotmail.com>
2025-02-04 11:51:31 +0100

    0-lookup.py

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 def lookup(obj):
-	"""
+    """
     Returns the list of available attributes and methods of an object.
-    
+
     Args:
         obj: The object to inspect.
-    
+
     Returns:
         List of attributes and methods available in the object.
     """
-    
-	return dir(obj)
+    return dir(obj)
