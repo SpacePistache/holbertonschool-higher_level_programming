b13644af1c3b930776166caaff4ebf0fb7365a80
SpacePistache <lukeholley@hotmail.com>
2025-02-05 10:59:05 +0100

    1-my_list.py

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" A class that inherits from a list """
+
+
+class Mylist(list):
+    """ Mylist inherits from list """
+
+
+def print_sorted(self):
+    """ Prints list in ascending order, no modification """
+
+    print(sorted(self))
