d385f38ff009239458f12dc1aecd7de717c7a303
SpacePistache <lukeholley@hotmail.com>
2025-02-05 12:22:57 +0100

    2-is_same_class.py

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+def is_same_class(obj, a_class):
+    """
+    Returns True if obj is exactly an instance of a_class False otherwise
+
+    Args:
+        obj: The object to check
+        a_class: The class to compare against
+
+    Returns:
+        boolean: True if obj is an instance of a_class, False otherwise
+    """
+    return type(obj) is a_class
