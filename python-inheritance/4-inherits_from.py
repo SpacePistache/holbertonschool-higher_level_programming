48851775ffd28eb96f690bef9467351039f367b8
SpacePistache <lukeholley@hotmail.com>
2025-02-05 13:13:58 +0100

    4-inherits_from.py

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module that returns True if the object is an instance of a class
+   that inherited (directly or indirectly)
+   from the specified class ; otherwise False."""
+
+
+def inherits_from(obj, a_class):
+    """
+    Returns True if the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class; otherwise False.
+
+    Args:
+        obj (object): The object to check.
+        a_class (type): The class to compare against.
+
+    Returns:
+        True if `obj` is an instance of a class that inherited from `a_class`,
+              False otherwise.
+    """
+    return isinstance(obj, a_class) and not issubclass(type(obj), a_class)
