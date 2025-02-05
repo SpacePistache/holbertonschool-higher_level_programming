d9527546525c1bdbcaa5f18965c29cd483c06f99
SpacePistache <lukeholley@hotmail.com>
2025-02-05 14:17:00 +0100

    7-base_geometry.py

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Module for geometric shape validation and area calculations.
+
+This module contains the BaseGeometry class, which provides methods
+for validating integer inputs and calculating geometric shape areas.
+"""
+
+
+class BaseGeometry:
+    """A class for geometric uses"""
+
+    def area(self):
+        """Raises an exception indicating the area method is not implemented.
+    This method is meant to be implemented in subclasses of BaseGeometry.
+    """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Checks if int is 0 or greater ergo a valid input
+           Args:
+                name (str): The name of the parameter (string).
+                value (int): The value to be validated (integer).
+           Raises:
+                  TypeError: If the value is not an integer.
+                  ValueError: If the value is less than or equal to 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class that inherits from BaseGeometry"""
+
+    def __init__(self, width, height):
+        """Initialize Rectangle instance.
+
+        Args:
+            width (int): width of the rectangle
+            height (int): height of the rectangle
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
