7f560e4595892b20d561f25b5fea65627f9b788b
SpacePistache <lukeholley@hotmail.com>
2025-02-05 13:23:02 +0100

    5-base_geometry.py

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""A module elaborating on the previous Geometric class"""
+
+
+def area(self):
+    """Function calculating area of geometric shapes"""
+    raise Exception("area() is not implemented")
