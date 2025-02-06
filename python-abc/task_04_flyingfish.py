f00fa0995d840d99597abfe18363a7bb32b74123
SpacePistache <lukeholley@hotmail.com>
2025-02-06 14:42:18 +0100

    task_04_flyingfish.py

--- python-abc/task_04_flyingfish.py
+++ python-abc/task_04_flyingfish.py
@@ -40,3 +40,16 @@ class FlyingFish(Fish, Bird):
     def habitat(self):
         """The flying fish doesn't compromise, it lives wherever it wants!"""
         print("The flying fish lives both in water and the sky!")
+
+
+if __name__ == "__main__":
+
+    flying_fish = FlyingFish()
+
+    print("Testing methods:")
+    flying_fish.fly()
+    flying_fish.swim()
+    flying_fish.habitat()
+
+    print("\nMethod Resolution Order:")
+    print(FlyingFish.mro())
