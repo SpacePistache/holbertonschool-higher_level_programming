74400d7705afbe35348869a41c364707aa93a06c
SpacePistache <lukeholley@hotmail.com>
2025-02-06 14:09:43 +0100

    task_03_countediterator.py

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""A module creating an iterator to count iterations"""
+
+
+class CountedIterator:
+    """An iterator that counts iterations"""
+
+    def __init__(self, iteration):
+        """Initialization of iteration counter"""
+        self.iterator = iter(iteration)
+        self.index = 0
+
+    def __iter__(self):
+        """Returns self as iterable"""
+        return self
+
+    def __next__(self):
+        """Moves on to next item and counts"""
+        try:
+            item = next(self.iterator)
+            self.count += 1
+            return item
+        except StopIteration:
+            raise
+
+
+def get_count(self):
+    """Return the total number of items iterated over."""
+    return self.count
