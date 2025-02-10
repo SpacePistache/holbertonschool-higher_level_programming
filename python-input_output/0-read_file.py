f7b22de7cded4fe2521e712762f1b49726eb0e92
SpacePistache <lukeholley@hotmail.com>
2025-02-10 20:05:09 +0100

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,11 +2,15 @@
 """This a module that reads a text file and prints it to stdout"""
 
 
-def write_file(filename="", text=""):
-    """A function that opens and reads the text in the .txt file"""
+def read_file(filename=""):
+    """
+        A function that opens and reads the text in the .txt file
+
+       Args:
+           filename: this'll be the file that is accessed and read
+    """
 
 
-with open("my_file_0.txt", "r", encoding="utf-8") as file:
-    content = file.read()
-print(content)
+with open(filename="", encoding="utf-8") as file:
+    print(file.read(), end="")
 file.close()
