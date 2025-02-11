3aec6394ac6edbb145de57f9972d483b88ddb336
SpacePistache <lukeholley@hotmail.com>
2025-02-11 11:31:17 +0100

    0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -11,6 +11,5 @@ def read_file(filename=""):
     """
 
 
-with open(filename="", encoding="utf-8") as file:
+with open(filename, "r", encoding="utf-8") as file:
     print(file.read(), end="")
-file.close()
