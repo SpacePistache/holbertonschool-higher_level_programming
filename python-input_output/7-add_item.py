c8f7a07e40ee9dc67790c9a062d2cd7beea47ea0
SpacePistache <lukeholley@hotmail.com>
2025-02-12 11:11:41 +0100

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """A module that adds, loads and saves from a previous file"""
 import sys
+from os.path import exists
+from importlib import import_module
+
 save_to_json_file = import_module("5-save_to_json_file").save_to_json_file
 load_from_json_file = import_module("6-load_from_json_file").load_from_json_file
 
