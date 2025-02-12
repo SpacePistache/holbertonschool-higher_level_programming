141132353e14a38552c37af4a6a54566cf6dc71f
SpacePistache <lukeholley@hotmail.com>
2025-02-12 10:16:56 +0100

    7-add_item.py

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A module that adds, loads and saves from a previous file"""
+import sys
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
+
+filename = "add_item.json"
+try:
+    items = load_from_json_file(filename)
+except FileNotFoundError:
+    items = []
+
+items.extend(sys.argv[1:])
+
+save_to_json_file(items, filename)
