30a49e6edffe0d62ed310033d406f8385105ae8c
SpacePistache <lukeholley@hotmail.com>
2025-02-13 10:13:25 +0100

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -2,10 +2,10 @@
 """A module that adds, loads and saves from a previous file"""
 import sys
 from os.path import exists
-from importlib import import_module
 
-save_to_json_file = import_module("5-save_to_json_file").save_to_json_file
-load_from_json_file = import_module("6-load_from_json_file").load_from_json_file
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 filename = "add_item.json"
 if exists(filename):
