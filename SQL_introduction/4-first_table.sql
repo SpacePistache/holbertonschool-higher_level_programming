906d21cbf8bc55a6e22cdae0ae290208a4b7c2fb
SpacePistache <lukeholley@hotmail.com>
2025-03-05 22:10:28 +0100

    deleted database creation and comment

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,9 +1,6 @@
 -- A scripting creating a table called 'first_table'
--- Recreated the previously deleted hbtn database
 -- columns: id(INT), name(VARCHAR(256))
 
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
-USE hbtn_0c_0;
 CREATE TABLE IF NOT EXISTS first_table (
 	id INT,
 	name VARCHAR(256)
