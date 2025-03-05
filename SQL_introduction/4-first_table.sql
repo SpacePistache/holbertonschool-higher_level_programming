b8c5e8bc03c98959f9ff12751cf76ea730204941
SpacePistache <lukeholley@hotmail.com>
2025-03-05 20:31:49 +0100

    4-first_table.sql added USE

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,7 @@
 -- A scripting creating a table called 'first_table'
 -- columns: id(INT), name(VARCHAR(256))
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+USE hbtn_0c_0;
 CREATE TABLE IF NOT EXISTS first_table (
 	id INT,
 	name, VARCHAR(256)
