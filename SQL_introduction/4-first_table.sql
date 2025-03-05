d0ce23d145e96c9011e55feb986393afbc5568e6
SpacePistache <lukeholley@hotmail.com>
2025-03-05 20:30:26 +0100

    4-first_table.sql recreated hbtn database

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,5 +1,6 @@
 -- A scripting creating a table called 'first_table'
 -- columns: id(INT), name(VARCHAR(256))
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 CREATE TABLE IF NOT EXISTS first_table (
 	id INT,
 	name, VARCHAR(256)
