849bd1106200b0b7d04d3ed090027b771be9dc9e
SpacePistache <lukeholley@hotmail.com>
2025-03-05 22:07:34 +0100

    4-first_table.sql added comment

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,4 +1,5 @@
 -- A scripting creating a table called 'first_table'
+-- Recreated the previously deleted hbtn database
 -- columns: id(INT), name(VARCHAR(256))
 
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
