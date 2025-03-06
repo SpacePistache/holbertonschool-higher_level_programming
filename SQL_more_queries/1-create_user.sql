80799bd2483fe322d7c05ef10d3585bd5cf098a5
SpacePistache <lukeholley@hotmail.com>
2025-03-06 15:49:27 +0100

    1-create_user.sql

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- Creates a user if inexistant and sets a password
+CREATE USER IF NOT EXIST 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd'
+
+--Grants all privileges to user_0d-1 on entire server
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
