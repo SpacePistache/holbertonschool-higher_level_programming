e3c1b7a11932c5c29159f81a8cdab4f181d92ea5
SpacePistache <lukeholley@hotmail.com>
2025-03-06 16:23:04 +0100

    removed grant options

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- A script creating a user and granting server wide access privileges
 DROP USER IF EXISTS 'user_0d_1'@'localhost';
 CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
