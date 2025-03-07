77f9cbfb79ec0384ddae0b0ca6b45a2bcc31bd33
SpacePistache <lukeholley@hotmail.com>
2025-03-07 11:27:09 +0100

    initial testing to see if kinda working

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,16 +5,14 @@ import mySQLdb
 import sys
 
 if __name__ == "__main__":
-	username = sys.argv[1]
-	password = sys.argv[2]
-	database = sys.argv[3]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-	db = mySQLdb.connect(
-		host="localhost",
-		port=3306,
-		user=username,
-		passwd=password,
-		db=database
-	)
-
-	
+    db = mySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
