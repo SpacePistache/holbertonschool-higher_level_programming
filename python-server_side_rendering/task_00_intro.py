cdcf0e854fe9d654c728bdc111796dc913b95935
SpacePistache <lukeholley@hotmail.com>
2025-03-21 14:00:26 +0100

    added print

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -5,20 +5,21 @@ def generate_invitations(template_content, attendees):
     for attendee in attendees:
         filename = "output_" + str(i) + ".txt"
         i += 1
-        if attendee ["name"]:
-            name = attendee ["name"] 
+        if attendee["name"]:
+            name = attendee["name"]
         else: name = "N/A"
-        if attendee ["event_title"]:
+        if attendee["event_title"]:
             event_title = attendee ["event_title"]
         else: "N/A"
-        if attendee ["event_date"]:
+        if attendee["event_date"]:
              event_date = attendee ["event_date"]
         else: "N/A"
-        if attendee ["event_location"]:
-            event_location = attendee ["event_location"]
+        if attendee["event_location"]:
+            event_location = attendee["event_location"]
         else: "N/A"
-        new_content = template_content.replace("{name}", name).replace("{event_title}", event_title).replace("{event_date}", event_date).replace("{event_location}", event_location)
+        new_content = template_content.replace("{name}", name)\
+            .replace("{event_title}", event_title).replace("{event_date}", event_date)\
+            .replace("{event_location}", event_location)
         with open(filename, "w") as file:
             file.write(new_content)
-    
-        
+            print(new_content)
