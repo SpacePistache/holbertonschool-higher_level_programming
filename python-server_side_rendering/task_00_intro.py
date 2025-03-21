1c67129c29da65fe808bb06a03a9f90199072251
SpacePistache <lukeholley@hotmail.com>
2025-03-21 17:25:44 +0100

    added error messages

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
 
 def generate_invitations(template_content, attendees):
+
+    if not isinstance(template_content, str):
+        print("Error: Template is not a valid string.")
+        return
+    if not template_content.strip():
+        print("Template is empty, no output files generated.")
+        return
+    if not isinstance(attendees, list) or not all(isinstance(a, dict) for a in attendees):
+        print("Error: Attendees should be a list of dictionaries.")
+        return
+    if not attendees:
+        print("No data provided, no output files generated.")
+        return
+
     i = 1
     for attendee in attendees:
         filename = "output_" + str(i) + ".txt"
@@ -17,8 +31,6 @@ def generate_invitations(template_content, attendees):
         if attendee["event_location"]:
             event_location = attendee["event_location"]
         else: "N/A"
-        if not isinstance(template_content, str):
-            print("Template is empty, no output files generated.")
         new_content = template_content.replace("{name}", name)\
             .replace("{event_title}", event_title).replace("{event_date}", event_date)\
             .replace("{event_location}", event_location)
