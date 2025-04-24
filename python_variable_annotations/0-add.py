89c34b4dadb42512a8cf7864947cd8c5e567ec69
SpacePistache <lukeholley@hotmail.com>
2025-04-24 09:53:36 +0200

    second commit, forgot to add annontations

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,3 +1,18 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Module for adding two float numbers.
+"""
+
+
 def add(a: float, b: float) -> float:
+    """
+    Add two float numbers and return the result.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+    float: The sum of a and b.
+    """
     return a + b
